772cf372b73b72329c48f64ac532b1b9f7127eb1
Maia Norville <norvilm1@matc.edu>
2026-01-25 21:32:51 -0600

    Complete Activity 1

---
+++ activity-1/scripts.js
@@ -0,0 +1,22 @@
+console.log("Hello, World!");
+document.getElementById("demo").innerHTML = "Hello, World!";
+let studentName = "Mai'a Norville";
+const age = "17";
+let isStudent = true;
+let emptyValue = null;
+let notAssigned;
+
+console.log("Student Name", studentName);
+console.log("Age", age);
+console.log("Is Student", isStudent);
+console.log("Empty Value", emptyValue);
+console.log("Not Assigned", notAssigned);
+
+console.log("Student Name", typeof studentName);
+console.log("Age", typeof age);
+console.log("Is Student", typeof isStudent);
+console.log("Empty Value", typeof emptyValue);
+console.log("Not Assigned", typeof notAssigned);
+
+studentName = "Zach White"
+console.log("Updated Student Name", studentName);
